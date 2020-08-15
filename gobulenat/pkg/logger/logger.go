ff398ddaf98850a94efff41a61a1420a22e88c58
nadavbm <nadavbm@outlook.com>
2020-08-15 22:58:08 +0200

    adding dev global logger

--- gobulenat/pkg/logger/logger.go
+++ gobulenat/pkg/logger/logger.go
@@ -4,7 +4,19 @@ import (
 	"go.uber.org/zap"
 )
 
-var Logger *zap.Logger
+type Logger struct {
+	*zap.Logger
+}
+
+func DevLogger() *Logger {
+	l, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	return &Logger{
+		Logger: l,
+	}
+}
 
 func init() {
 	Logger, _ := zap.NewDevelopment()
