97f812192b01bc63086a34ac08a45f24e7db285f
nadavbm <nadavbm@outlook.com>
2020-09-27 13:22:06 +0200

    removing init fron main

--- api/run/main.go
+++ api/run/main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"database/sql"
-
-	_ "github.com/lib/pq"
-
-	"github.com/nadavbm/gobulenat/api/dat"
 	"github.com/nadavbm/gobulenat/api/server"
 	"github.com/nadavbm/gobulenat/pkg/logger"
 )
 
-var db *sql.DB
-
-func init() {
-	logger := logger.DevLogger()
-
-	dat.InitDB(logger)
-}
-
 func main() {
 	l := logger.DevLogger()
+
 	s := server.NewServer(l)
+	//s := server.Server{}
 	err := s.Run()
 	if err != nil {
 		panic(err)
