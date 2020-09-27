d04bee394eb3950192e8901030f38c53de9f09dc
nadavbm <nadavbm@outlook.com>
2020-09-27 13:23:17 +0200

    adding additional methods for login and signup, removing pages which we don't need yet

--- api/server/server.go
+++ api/server/server.go
@@ -1,11 +1,17 @@
 package server
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/nadavbm/gobulenat/api/dat"
 	"github.com/nadavbm/gobulenat/pkg/logger"
+
+	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
 
 type Server struct {
 	Logger     *logger.Logger
@@ -22,14 +28,26 @@ func NewServer(logger *logger.Logger) *Server {
 	return server
 }
 
+func init() {
+	dat.InitDB()
+}
+
 func (s *Server) Run() error {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/home", HomePage)
-	r.HandleFunc("/signup", SignupHandler)
-	r.HandleFunc("/", LoginHandler)
-	r.HandleFunc("/about", AboutPage)
+	//r.HandleFunc("/home", HomePage)
+	r.HandleFunc("/signup", SignupHandler).Methods("POST", "GET")
+	r.HandleFunc("/", LoginHandler).Methods("POST", "GET")
+	//r.HandleFunc("/about", AboutPage)
 	r.HandleFunc("/logout", LogoutHandler)
+
+	/* examples:
+	   router.HandleFunc("/api/user/{id}", middleware.GetUser).Methods("GET", "OPTIONS")
+	   router.HandleFunc("/api/user", middleware.GetAllUser).Methods("GET", "OPTIONS")
+	   router.HandleFunc("/api/newuser", middleware.CreateUser).Methods("POST", "OPTIONS")
+	   router.HandleFunc("/api/user/{id}", middleware.UpdateUser).Methods("PUT", "OPTIONS")
+	   router.HandleFunc("/api/deleteuser/{id}", middleware.DeleteUser).Methods("DELETE", "OPTIONS")
+	*/
 
 	r.PathPrefix("/home/rodriguez/go/src/github.com/nadavbm/gobulenat/api/server/static/").Handler(http.StripPrefix("/home/rodriguez/go/src/github.com/nadavbm/gobulenat/api/server/static/", http.FileServer(http.Dir("/home/rodriguez/go/src/github.com/nadavbm/gobulenat/api/server/static/"))))
 
